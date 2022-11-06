d3dc3e62f2f96d2e2080669c22176f599ae94717
ym97 <manisha.yara@yahoo.com>
2022-11-06 13:10:13 -0500

    Add voice

--- apiGateway-js-sdk/index.js
+++ apiGateway-js-sdk/index.js
@@ -1,168 +1,33 @@
-/*
- * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-var apigClientFactory = {};
-apigClientFactory.newClient = function (config) {
-    var apigClient = { };
-    if(config === undefined) {
-        config = {
-            accessKey: '',
-            secretKey: '',
-            sessionToken: '',
-            region: '',
-            apiKey: undefined,
-            defaultContentType: 'application/json',
-            defaultAcceptType: 'application/json'
-        };
-    }
-    if(config.accessKey === undefined) {
-        config.accessKey = '';
-    }
-    if(config.secretKey === undefined) {
-        config.secretKey = '';
-    }
-    if(config.apiKey === undefined) {
-        config.apiKey = '';
-    }
-    if(config.sessionToken === undefined) {
-        config.sessionToken = '';
-    }
-    if(config.region === undefined) {
-        config.region = 'us-east-1';
-    }
-    //If defaultContentType is not defined then default to application/json
-    if(config.defaultContentType === undefined) {
-        config.defaultContentType = 'application/json';
-    }
-    //If defaultAcceptType is not defined then default to application/json
-    if(config.defaultAcceptType === undefined) {
-        config.defaultAcceptType = 'application/json';
-    }
-
-    
-    // extract endpoint and path from url
-    var invokeUrl = 'https://iav31pdv15.execute-api.us-east-1.amazonaws.com/beta';
-    var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
-    var pathComponent = invokeUrl.substring(endpoint.length);
-
-    var sigV4ClientConfig = {
-        accessKey: config.accessKey,
-        secretKey: config.secretKey,
-        sessionToken: config.sessionToken,
-        serviceName: 'execute-api',
-        region: config.region,
-        endpoint: endpoint,
-        defaultContentType: config.defaultContentType,
-        defaultAcceptType: config.defaultAcceptType
-    };
-
-    var authType = 'NONE';
-    if (sigV4ClientConfig.accessKey !== undefined && sigV4ClientConfig.accessKey !== '' && sigV4ClientConfig.secretKey !== undefined && sigV4ClientConfig.secretKey !== '') {
-        authType = 'AWS_IAM';
-    }
-
-    var simpleHttpClientConfig = {
-        endpoint: endpoint,
-        defaultContentType: config.defaultContentType,
-        defaultAcceptType: config.defaultAcceptType
-    };
-
-    var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
-    
-    
-    
-    apigClient.addBucketKeyPut = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
-        apiGateway.core.utils.assertParametersDefined(params, ['key', 'bucket', 'x-amz-meta-customLabels'], ['body']);
-        
-        var addBucketKeyPutRequest = {
-            verb: 'put'.toUpperCase(),
-            path: pathComponent + uritemplate('/add/{bucket}/{key}').expand(apiGateway.core.utils.parseParametersToObject(params, ['key', 'bucket', ])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, ['x-amz-meta-customLabels']),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
-            body: body
-        };
-        
-        
-        return apiGatewayClient.makeRequest(addBucketKeyPutRequest, authType, additionalParams, config.apiKey);
-    };
-    
-    
-    apigClient.addBucketKeyOptions = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
-        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
-        var addBucketKeyOptionsRequest = {
-            verb: 'options'.toUpperCase(),
-            path: pathComponent + uritemplate('/add/{bucket}/{key}').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, []),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
-            body: body
-        };
-        
-        
-        return apiGatewayClient.makeRequest(addBucketKeyOptionsRequest, authType, additionalParams, config.apiKey);
-    };
-    
-    
-    apigClient.searchGet = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
-        apiGateway.core.utils.assertParametersDefined(params, ['q'], ['body']);
-        
-        var searchGetRequest = {
-            verb: 'get'.toUpperCase(),
-            path: pathComponent + uritemplate('/search').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, []),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['q']),
-            body: body
-        };
-        
-        
-        return apiGatewayClient.makeRequest(searchGetRequest, authType, additionalParams, config.apiKey);
-    };
-    
-    
-    apigClient.searchOptions = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
-        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
-        var searchOptionsRequest = {
-            verb: 'options'.toUpperCase(),
-            path: pathComponent + uritemplate('/search').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, []),
-            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
-            body: body
-        };
-        
-        
-        return apiGatewayClient.makeRequest(searchOptionsRequest, authType, additionalParams, config.apiKey);
-    };
-    
-
-    return apigClient;
-};
-
-
-
 var apigClient = apigClientFactory.newClient();
-// window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-//window.SpeechRecognition = window.webkitSpeechRecognition;
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const query = document.getElementById("query");
+
+startRecognition = () => {
+  if (SpeechRecognition !== undefined) {
+    let recognition = new SpeechRecognition();
+
+    recognition.onstart = () => {
+      status.innerHTML = 'Listening';
+      output.classList.add("hide");
+    };
+
+    recognition.onspeechend = () => {
+      status.innerHTML = 'Stopped';
+      recognition.stop();
+    };
+
+    recognition.onresult = (result) => {
+      console.log(result.results[0][0].transcript)
+      query.value = result.results[0][0].transcript
+    };
+    recognition.start();
+  } else {
+    status.innerHTML = "Voice is not supported";
+  }
+};
 
 function searchByVoice(){
     var query = document.getElementById("query");
