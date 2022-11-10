3de268df887d2423e7f82a33f681ab297b694f1b
ym97 <manisha.yara@yahoo.com>
2022-11-10 16:19:47 -0500

    for cloud

--- apiGateway-js-sdk/apigClient.js
+++ apiGateway-js-sdk/apigClient.js
@@ -104,12 +104,12 @@ apigClientFactory.newClient = function (config) {
     apigClient.addBucketKeyOptions = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
         
-        apiGateway.core.utils.assertParametersDefined(params, ['Content-Type', 'Accept', 'x-amz-meta-customLabels', 'x-api-key', 'key', 'bucket'], ['body']);
+        apiGateway.core.utils.assertParametersDefined(params, ['Content-Type', 'Accept', 'x-amz-meta-customLabels', 'x-api-key'], ['body']);
         
         var addBucketKeyOptionsRequest = {
             verb: 'options'.toUpperCase(),
-            path: pathComponent + uritemplate('/add/{bucket}/{key}').expand(apiGateway.core.utils.parseParametersToObject(params, ['key', 'bucket'])),
-            headers: apiGateway.core.utils.parseParametersToObject(params, ['Content-Type', 'Accept', 'x-amz-meta-customLabels', 'x-api-key', ]),
+            path: pathComponent + uritemplate('/add/{bucket}/{key}').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, ['Content-Type', 'Accept', 'x-amz-meta-customLabels', 'x-api-key']),
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
